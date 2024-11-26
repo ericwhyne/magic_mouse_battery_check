35492b554a427e7a9a530a63a71bdffa4f98f25a
Eric Whyne <ericwhyne@gmail.com>
2024-11-26 11:04:28 -0500

    initial

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# magic_mouse_battery_check
+# Magic Mouse Low Battery Alert
+
+## What
+This script is designed to be placed in a daily crontab and alerts with a popup if the magic mouse battery falls below 25%. 
+
+## Why
+OSX notifications are too noisy for me see mouse battery warnings. The mouse can't be used while plugged in so a dead mouse battery is disruptive. 
---
+++ checkmouse.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# Creates an OSX popup window if magic mouse battery drops below %25
+# ericwhyne@datamachines.com
+# Add this to crontab: 0 7 * * * /path/to/this/script.sh
+NOTIFY_FILE="/tmp/mouse_battery_low"
+export DISPLAY=:0
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin"
+battery_level=$(ioreg -c AppleDeviceManagementHIDEventService -r | \
+  grep -i '"BatteryPercent" =' | awk '{print $NF}' | head -n 1)
+echo "Mouse Battery level: $battery_level"
+if [ "$battery_level" -lt 25 ]; then
+    if [ ! -f "$NOTIFY_FILE" ]; then
+        touch "$NOTIFY_FILE"
+        sync
+        osascript -e "display dialog \"Mouse battery low: $battery_level%.\" \
+                      with title \"Low Battery Warning\" \
+                      buttons {\"OK\"} \
+                      default button \"OK\"" > /dev/null 2>&1 &
+    fi
+else
+    [ -f "$NOTIFY_FILE" ] && rm "$NOTIFY_FILE"
+fi
